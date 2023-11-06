97c809da8651d11137533af725fc84f02ab20826
Mahmoudi Mohamed Amine <mohamed.mahmoudi@ensia.edu.dz>
2023-11-06 23:34:19 +0100

    Starting_Calendar_Board

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,7 +1,27 @@
 import "./Calendar.css";
+import Year from "./Year";
+import Month from "./Month";
+import Week from "./Week";
+import Day from "./Day";
 
 export default function Calendar() {
-  return (
-  <div className="Calendar"></div>
-  );
+  const key = "Year";
+  let Calendar_Mode;
+
+  {
+    switch (key) {
+      case "Year":
+        Calendar_Mode = <Year />;
+      case "Month":
+        Calendar_Mode = <Month />;
+      case "Week":
+        Calendar_Mode = <Week />;
+      case "Day":
+        Calendar_Mode = <Day />;
+      default:
+        Calendar_Mode = <Year />;
+    }
+  }
+
+  return <div className="Calendar">{Calendar_Mode}</div>;
 }
